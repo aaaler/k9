1ba08cf379f1342092dfdf3b9de045088fb2a2de
aaaler <aaaler@gmail.com>
2014-10-17 01:10:27 +0400

    joystick deadzones

--- kpilot/kpilotgui.py
+++ kpilot/kpilotgui.py
@@ -4,7 +4,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.uix.stencilview import StencilView
 from kivy.uix.slider import Slider
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, ListProperty
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.graphics import Color, Ellipse, Line
@@ -110,9 +110,8 @@ Builder.load_string("""
                     Joystick:
                         size_hint: 1, 1 
                         id: joy1			
-                        on_touch_down: root.TracksActivate()
-                        on_touch_move: root.TracksActivate()
-                        on_touch_up: root.TracksDeactivate()
+                        on_pos: root.TracksMove ()
+                        on_touch_up: root.TracksStop ()
 
     TabbedPanelItem:
         text: 'FPV'
@@ -205,6 +204,16 @@ Builder.load_string("""
         Rectangle:
             pos: self.pos
             size: self.size
+        Color:
+            rgba: 0, 1, 0, .1
+        Rectangle:
+            pos: self.x, self.center_y - (self.height*self.deadzone)
+            size: self.width , (self.height*self.deadzone*2)
+        Rectangle:
+            pos: self.center_x - (self.width*self.deadzone), self.y
+            size: self.width*0.1, self.height 
+
+
     Label:
         id: joycap
         text: '+'
@@ -243,24 +252,10 @@ class MainTabs(TabbedPanel):
     label1 = ObjectProperty()
     label2 = ObjectProperty()
 
-#    
-#    def TrackButtonDown (self, cmd): 
-#        pilot.direction = cmd
-#        self.SetSpeeds (self)    
+    def TracksMove (self):
+        pilot.send ("TRACKS %0.2f %0.2f" % (self.joy1.pos[0],self.joy1.pos[1]))
 
-#    def SetSpeeds (self, smth=""): 
-#        pilot.speed = int (self.speed.value)
-#        pilot.rotspeed = int (self.rot_speed.value)
-    
-#    def TrackButtonUp (self, cmd):
-#        if pilot.direction == cmd : 
-#            pilot.send ("STOP")
-#            pilot.direction = "";
-    def TracksActivate (self):
-	if (self.joy1.jy != 0 or self.joy1.jx != 0 ):
-          pilot.send ("TRACKS %0.2f %0.2f" % (self.joy1.jx,self.joy1.jy))
-
-    def TracksDeactivate (self):
+    def TracksStop (self):
         pilot.send ("TRACKS 0 0")
 
     def SetServo1 (self, smth=""): 
@@ -292,8 +287,11 @@ class MainTabs(TabbedPanel):
         
 class Joystick(Widget):
 #StencilView
+
     jx = 0.;
     jy = 0.;
+    deadzone = 0.05; 
+    pos = ListProperty([0, 0])
     def on_height(self, pos, smth='123'):
         self.resetcap(); 
     def on_width(self, pos, smth='123'):
@@ -303,11 +301,16 @@ class Joystick(Widget):
           self.movecap(touch);
           touch.grab(self)
           return True;
+
     def on_touch_move(self, touch):
-      	if self.collide_point(touch.x,touch.y):
-          if touch.grab_current == self:
-            self.movecap(touch);
-            return True
+        if touch.grab_current == self:
+          if touch.x > self.width + self.x: touch.x = self.width + self.x
+          if touch.x < self.x: touch.x = self.x
+          if touch.y > self.height + self.y: touch.y = self.height + self.y
+          if touch.y < self.y: touch.y = self.y
+
+          self.movecap(touch);
+          return True
 
     def on_touch_up(self, touch):
         if touch.grab_current == self:
@@ -315,19 +318,30 @@ class Joystick(Widget):
             return True
 
 
+    def on_pos(self,joy,pos):
+        pass
+
     def resetcap(self):
-          self.joycap.center_x = self.center_x;
-          self.joycap.center_y = self.center_y;
-          self.jx = 0.;
-          self.jy = 0.;
-          self.coordslabel.text = "x:%0.3f y: %0.3f" % (self.jx ,self.jy) 
+      self.joycap.center_x = self.center_x;
+      self.joycap.center_y = self.center_y;
+      self.jx = 0.;
+      self.jy = 0.;
+      self.coordslabel.text = "x:%0.3f y: %0.3f" % (self.jx ,self.jy) 
+      self.pos = (self.jx ,self.jy)
 
     def movecap(self, touch):
-          self.joycap.center_x = touch.x;
-          self.joycap.center_y = touch.y;
-	  self.jx = ((touch.x - self.x) - self.width/2)/self.width*2
-	  self.jy = ((touch.y - self.y) - self.height/2)/self.height*2
-          self.coordslabel.text = "x:%0.3f y: %0.3f" % (self.jx ,self.jy) 
+      if touch.x > self.center_x - (self.width*self.deadzone) and touch.x < self.center_x + (self.width*self.deadzone):
+        touch.x = self.center_x;
+      if touch.y > self.center_y - (self.height*self.deadzone) and touch.y < self.center_y + (self.height*self.deadzone):
+        touch.y = self.center_y;
+
+      self.joycap.center_x = touch.x;
+      self.joycap.center_y = touch.y;
+      self.jx = ((touch.x - self.x) - self.width/2)/self.width*2
+      self.jy = ((touch.y - self.y) - self.height/2)/self.height*2
+
+      self.coordslabel.text = "x:%0.3f y: %0.3f" % (self.jx ,self.jy) 
+      self.pos = (self.jx ,self.jy)
 
 class TouchLiveSlider(Slider):
     '''Custom slider class for having his index and custom graphics defined in
